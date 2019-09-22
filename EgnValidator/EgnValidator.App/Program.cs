082cbaf39c903d291103dec835fcc8982128d648
MomchilAngelovv <momchilangelov0040@gmail.com>
2019-09-22 21:22:00 +0300

    First initialization -> Tests and app projects

---
+++ EgnValidator/EgnValidator.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EgnValidator.App
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
